baf34c279243c7d1699354117b80f2dbe696b65b
Carra78 <carra78@hotmail.com>
2018-02-23 10:47:46 +0000

    Testing data write

---
+++ TestHotTipster/testDataWriterSqlite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotTipster.DataWriter;
+
+namespace TestHotTipster
+{
+	[TestClass]
+	public class testDataWriterSqlite
+	{
+		[TestMethod]
+		public void TestCreateDatabase()
+		{
+			string filepath = "test";
+			WriteToSQLite dataWriter = new WriteToSQLite();
+			dataWriter.CreateDatabase();
+			string actualFilePath = string.Format(Directory.GetCurrentDirectory() + Path.PathSeparator + "HotTipster.db");
+			//Assert.AreEqual(filepath, actualFilePath);
+
+		}
+	}
+}
